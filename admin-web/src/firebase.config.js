98504dc32f968fe9edab64caba9f0d0103ea12e7
Quach Hai Nam <quachhainam@email.com>
2026-05-09 11:32:12 +0700

    feat: implement Firebase initialization and service setup

--- admin-web/src/firebase.config.js
+++ admin-web/src/firebase.config.js
@@ -1,6 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +13,4 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// eslint-disable-next-line no-unused-vars
-const analytics = getAnalytics(app);
+export default firebaseConfig;
