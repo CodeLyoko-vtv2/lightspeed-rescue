287533aed26b997fb2acf5d136f68a8be6b50554
Quach Hai Nam <quachhainam@email.com>
2026-05-09 15:52:33 +0700

    feat(sos): implement onSosUpdated trigger for event multiplexing

--- functions/src/sos/index.js
+++ functions/src/sos/index.js
@@ -1,8 +1,10 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const { onSosCreated } = require("./onSosCreated");
+const { onSosUpdated } = require("./onSosUpdated");
 
 exports.sosPing = onRequest((req, res) => {
   res.json({ ok: true, service: "sos" });
 });
 
 exports.onSosCreated = onSosCreated;
+exports.onSosUpdated = onSosUpdated;
