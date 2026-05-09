cc93161683dd5c1487ca903c639abe0d607e870e
Quach Hai Nam <quachhainam@email.com>
2026-05-09 15:04:47 +0700

    feat(functions): init package.json and basic ping endpoints

---
+++ functions/eslint.config.js
@@ -0,0 +1,16 @@
+const globals = require("globals");
+
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
+    }
+  }
+];
